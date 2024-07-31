eed2e7b6c2ce5fb0bdf2ed2a6be15597d8b5e32a
Eric Codrea <codrea.eric@gmail.com>
2024-07-31 14:15:05 -0500

    qserver now works

--- scripts/bs_qserver_startup.py
+++ scripts/bs_qserver_startup.py
@@ -4,12 +4,14 @@ Configure for data collection using bluesky-queueserver.
 
 import logging
 
+import databroker
 from sim_bs_instrument.callbacks import *  # noqa
 from sim_bs_instrument.devices import *  # noqa
-from sim_bs_instrument.initialize_bs_tools import RE
-from sim_bs_instrument.initialize_bs_tools import cat
 from sim_bs_instrument.plans import *  # noqa
+from sim_bs_instrument.utils.catalog import load_catalog
 from sim_bs_instrument.utils.epics_tools import set_control_layer
+from sim_bs_instrument.utils.iconfig_loader import iconfig
+from sim_bs_instrument.utils.run_engine import run_engine
 
 # guides choice of module to import cat
 
@@ -18,6 +20,19 @@ logger = logging.getLogger(__name__)
 logger.info(__file__)
 print(__file__)
 
-RE.subscribe(cat.v1.insert)
+# Connect with our mongodb database
+catalog_name = iconfig.get("DATABROKER_CATALOG", "training")
+try:
+    # We don't actually run this part because it is unable to find a yaml corresponding to the catalog name. For this to execute the yaml has to be store in:
+    # import databroker; print(databroker.catalog_search_path())
+    # https://blueskyproject.io/databroker/reference/configuration.html?highlight=search%20path
+    cat = load_catalog(catalog_name)
+    logger.info("using databroker catalog '%s'", cat.name)
+except KeyError:
+    cat = databroker.temp().v2
+    logger.info("using TEMPORARY databroker catalog '%s'", cat.name)
+
+# Set up a RunEngine.
+RE = run_engine()
 
 set_control_layer("PyEpics")
